023aeb65a3ae70c6356efc76f47112d6bf878f8f
genre0928 <genre0928@naver.com>
2026-02-04 11:59:44 +0900

    structure 브랜치 병합

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,4 @@
+// 사용자 URL의 위치에 따라 렌더링할 내용을 정의하여 React Router가 찾아서 렌더링할 수 있도록 함
 import { type RouteConfig, index } from "@react-router/dev/routes";
 
 export default [index("routes/home.tsx")] satisfies RouteConfig;
