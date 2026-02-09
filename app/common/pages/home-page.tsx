3fb93336a886cb00dd571885936691d77cffa7eb
genre0928 <genre0928@naver.com>
2026-02-09 12:19:14 +0900

    feat:PostCard 컴포넌트 구현

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -1,5 +1,8 @@
+import { Link } from "react-router";
+import { Button } from "../components/ui/button";
 import type { Route } from "./+types/home-page";
 import { ProductCard } from "~/features/products/components/product-card";
+import { PostCard } from "~/features/community/post-card";
 
 export const meta: Route.MetaFunction = () => {
   return [
@@ -10,7 +13,8 @@ export const meta: Route.MetaFunction = () => {
 
 export default function HomePage() {
   return (
-    <div className="px-20">
+    <div className="px-20 space-y-40">
+      {/* 오늘의 제품 */}
       <div className="grid grid-cols-3 gap-4">
         <div>
           <h2 className="text-5xl font-bold leading-tight tracking-tight">
@@ -19,18 +23,40 @@ export default function HomePage() {
           <p className="text-xl font-light text-foreground">
             커뮤니티에서 가장 인기 있는 제품을 확인해보세요.
           </p>
+          <Button variant="link" asChild className="text-lg p-0">
+            <Link to="/products/leaderboard">모든 리더보기 보러가기 →</Link>
+          </Button>
         </div>
+        <ProductCard
+          productId="productId"
+          name="제품명"
+          description="제품 설명"
+          commentCount={10}
+          viewCount={10}
+          likeCount={10}
+          isLiked={false}
+        />
+      </div>
+      {/* 토론 */}
+      <div className="grid grid-cols-3 gap-4">
         <div>
-          <ProductCard
-            productId="productId"
-            name="제품명"
-            description="제품 설명"
-            commentCount={10}
-            viewCount={10}
-            likeCount={10}
-            isLiked={false}
-          />
+          <h2 className="text-5xl font-bold leading-tight tracking-tight">
+            오늘의 토론
+          </h2>
+          <p className="text-xl font-light text-foreground">
+            커뮤니티에서 가장 인기 있는 토론을 확인해보세요.
+          </p>
+          <Button variant="link" asChild className="text-lg p-0">
+            <Link to="/community">모든 토론 보러가기 →</Link>
+          </Button>
         </div>
+        <PostCard
+          postId="postId"
+          title="토론 제목"
+          author="작성자"
+          category="카테고리"
+          timeAgo="12시간 전"
+        />
       </div>
     </div>
   );
