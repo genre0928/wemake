ca70c853e0804b07beeb52e49f56a30a6e11382a
genre0928 <genre0928@naver.com>
2026-02-13 12:23:29 +0900

    feat:카테고리/상세카테고리 페이지 구현

--- app/features/products/pages/categories-page.tsx
+++ app/features/products/pages/categories-page.tsx
@@ -1,3 +1,31 @@
+import { Hero } from "~/common/components/hero";
+import type { Route } from "./+types/categories-page";
+import { CategoryCard } from "../components/category-card";
+
+export const meta: Route.MetaFunction = () => {
+  return [
+    { title: "카테고리 | Wemake" },
+    { name: "description", content: "카테고리 페이지" },
+  ];
+};
+
 export default function CategoriesPage() {
-  return <div>Categories</div>;
+  return (
+    <div className="space-y-10">
+      <Hero
+        title="카테고리 모음"
+        description="카테고리별로 제품을 확인해보세요"
+      />
+      <div className="grid grid-cols-4 gap-10">
+        {Array.from({ length: 10 }).map((_, index) => (
+          <CategoryCard
+            key={`categoryId-${index}`}
+            id={`categoryId-${index}`}
+            name="카테고리 이름"
+            description="카테고리 설명"
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
--- app/features/products/pages/category-page.tsx
+++ app/features/products/pages/category-page.tsx
@@ -1,3 +1,36 @@
+import { Hero } from "~/common/components/hero";
+import { ProductCard } from "../components/product-card";
+import type { Route } from "./+types/category-page";
+import ProductPagination from "~/common/components/product-pagination";
+
+export const meta: Route.MetaFunction = ({ params }) => {
+  const { category } = params;
+  return [
+    { title: `카테고리 이름 | Wemake` },
+    { name: "description", content: `카테고리 이름 제품을 확인해보세요` },
+  ];
+};
+
 export default function CategoryPage() {
-  return <div>Category</div>;
+  return (
+    <div className="space-y-10">
+      <Hero title={"카테고리 이름"} description={"카테고리 설명"} />
+
+      <div className="space-y-5 w-full max-w-3xl mx-auto">
+        {Array.from({ length: 11 }).map((_, index) => (
+          <ProductCard
+            key={`productId-${index}`}
+            productId={`productId-${index}`}
+            name="제품명"
+            description="제품 설명"
+            commentCount={10}
+            viewCount={10}
+            likeCount={10}
+            isLiked={false}
+          />
+        ))}
+      </div>
+      <ProductPagination totalPages={10} />
+    </div>
+  );
 }
