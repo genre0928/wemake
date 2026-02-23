b2e5d8a5af8a9d130da8a43e556959be2f55afc1
genre0928 <genre0928@naver.com>
2026-02-23 17:36:36 +0900

    feat:리뷰 작성 기능 구현

--- app/features/products/pages/new-product-review-page.tsx
+++
@@ -1,3 +0,0 @@
-export default function NewProductReviewPage() {
-  return <div>NewProductReviewPage</div>;
-}
--- app/features/products/pages/product-reviews-page.tsx
+++ app/features/products/pages/product-reviews-page.tsx
@@ -1,14 +1,25 @@
 import { Button } from "~/common/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "~/common/components/ui/dialog";
 import { ReviewCard } from "~/features/products/components/review-card";
+import CreateReviewDialog from "../components/create-review-dialog";
 
 export default function ProductReviewsPage() {
   return (
-    <div>
+    <Dialog>
       <div className="max-w-1/2 space-y-5">
         {/* 리뷰 총 갯수와 리뷰 작성하기 섹션 */}
         <div className="flex justify-between items-center">
           <h2>10개의 리뷰</h2>
-          <Button variant="secondary">리뷰 작성하기</Button>
+          <DialogTrigger>
+            <Button variant="secondary">리뷰 작성하기</Button>
+          </DialogTrigger>
         </div>
         {/* 작성된 리뷰 섹션 */}
         <div className="space-y-20">
@@ -20,8 +31,18 @@ export default function ProductReviewsPage() {
             dateText="5일 전"
             showActions
           />
+          <ReviewCard
+            displayName="John Doe"
+            username="username"
+            rating={2}
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
+            dateText="5일 전"
+            showActions
+          />
         </div>
+        {/* 리뷰 작성하기 다이얼로그 섹션 */}
+        <CreateReviewDialog />
       </div>
-    </div>
+    </Dialog>
   );
 }
