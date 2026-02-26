999d713b69091ac302587853f071c09c7e280301
genre0928 <genre0928@naver.com>
2026-02-26 12:42:24 +0900

    docs:readme문서 수정

--- app/features/community/components/reply-card.tsx
+++ app/features/community/components/reply-card.tsx
@@ -8,6 +8,7 @@ import {
   AvatarImage,
 } from "~/common/components/ui/avatar";
 import { Button } from "~/common/components/ui/button";
+import { cn } from "~/lib/utils";
 
 export interface ReplyCardProps {
   nickname: string;
@@ -31,13 +32,13 @@ export function ReplyCard({
     setReplying((prev) => !prev);
   };
   return (
-    <div className="flex flex-col gap-5 w-2/3">
+    <div className={cn("flex flex-col gap-5")}>
       <div className="flex gap-5">
         <Avatar className="size-10">
           <AvatarImage src={avatarSrc} />
           <AvatarFallback>{avatarFallback}</AvatarFallback>
         </Avatar>
-        <div className="flex flex-col gap-5 text-sm text-muted-foreground w-full">
+        <div className="flex flex-col gap-5 text-sm text-muted-foreground w-2/3">
           <div className="flex flex-col gap-3">
             <div className="flex items-center gap-5">
               <Link to={`/users/${nickname}`}>
@@ -76,14 +77,12 @@ export function ReplyCard({
             </Form>
           )}
           {topLevel && (
-            <div className="flex flex-col gap-5">
-              <ReplyCard
-                nickname="닉네임"
-                content="나는 이 글의 내용에 대해 전적으로 동의해, 왜냐하면 나도 비슷한 경험이 있기 때문이야"
-                timeAgo="1분 전"
-                topLevel={false}
-              />
-            </div>
+            <ReplyCard
+              nickname="닉네임"
+              content="나는 이 글의 내용에 대해 전적으로 동의해, 왜냐하면 나도 비슷한 경험이 있기 때문이야"
+              timeAgo="1분 전"
+              topLevel={false}
+            />
           )}
         </div>
       </div>
