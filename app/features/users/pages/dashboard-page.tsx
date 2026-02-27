a9a4626e7caf09475706f08635fdd641ef42bbb8
genre0928 <genre0928@naver.com>
2026-02-27 15:14:01 +0900

    feat:상품분석 대시보드 구현

--- app/features/users/pages/dashboard-page.tsx
+++ app/features/users/pages/dashboard-page.tsx
@@ -1,3 +1,75 @@
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "~/common/components/ui/card";
+import {
+  ChartContainer,
+  ChartTooltip,
+  type ChartConfig,
+} from "~/common/components/ui/chart";
+import { LineChart } from "recharts";
+import { CartesianGrid } from "recharts";
+import { XAxis } from "recharts";
+import { ChartTooltipContent } from "~/common/components/ui/chart";
+import { Line } from "recharts";
+
+const chartData = [
+  { month: "January", views: 186 },
+  { month: "February", views: 305 },
+  { month: "March", views: 237 },
+  { month: "April", views: 73 },
+  { month: "May", views: 209 },
+  { month: "June", views: 214 },
+];
+const chartConfig = {
+  views: {
+    label: "👁️",
+    color: "var(--color-primary)",
+  },
+} as ChartConfig;
+
 export default function DashboardPage() {
-  return null;
+  return (
+    <div className="space-y-5">
+      <h1 className="text-2xl font-bold">대시보드</h1>
+      <Card className="w-1/2">
+        <CardHeader>
+          <CardTitle>방문자 수</CardTitle>
+          <CardContent>
+            <ChartContainer config={chartConfig}>
+              <LineChart
+                accessibilityLayer
+                data={chartData}
+                margin={{
+                  left: 12,
+                  right: 12,
+                }}
+              >
+                <XAxis
+                  dataKey="month"
+                  tickLine={false}
+                  axisLine={false}
+                  tickMargin={8}
+                  tickFormatter={(value) => value.slice(0, 3)}
+                />
+                <ChartTooltip
+                  cursor={false}
+                  content={<ChartTooltipContent hideLabel />}
+                />
+                <Line
+                  dataKey="views"
+                  type="natural"
+                  stroke="var(--color-views)"
+                  strokeWidth={2}
+                  dot={false}
+                />
+              </LineChart>
+            </ChartContainer>
+          </CardContent>
+        </CardHeader>
+      </Card>
+    </div>
+  );
 }
--- app/features/users/pages/dashboard-product-page.tsx
+++ app/features/users/pages/dashboard-product-page.tsx
@@ -1,3 +1,81 @@
+import { Area, AreaChart, Line, LineChart, XAxis } from "recharts";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "~/common/components/ui/card";
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+  type ChartConfig,
+} from "~/common/components/ui/chart";
+
+const chartData = [
+  { month: "January", views: 186, visitior: 780 },
+  { month: "February", views: 305, visitior: 350 },
+  { month: "March", views: 237, visitior: 572 },
+  { month: "April", views: 73, visitior: 66 },
+  { month: "May", views: 209, visitior: 81 },
+  { month: "June", views: 214, visitior: 158 },
+];
+const chartConfig = {
+  views: {
+    label: "👁️",
+    color: "var(--color-chart-1)",
+  },
+  visitior: {
+    label: "👤",
+    color: "var(--color-chart-2)",
+  },
+} as ChartConfig;
+
 export default function DashboardProductPage() {
-  return null;
+  return (
+    <div className="space-y-5">
+      <h1 className="text-2xl font-bold">상품 판매 분석</h1>
+      <Card className="w-1/2">
+        <CardHeader>
+          <CardTitle>방문자 수</CardTitle>
+          <CardContent>
+            <ChartContainer config={chartConfig}>
+              <AreaChart
+                accessibilityLayer
+                data={chartData}
+                margin={{
+                  left: 12,
+                  right: 12,
+                }}
+              >
+                <XAxis
+                  dataKey="month"
+                  tickLine={false}
+                  axisLine={false}
+                  tickMargin={8}
+                  tickFormatter={(value) => value.slice(0, 3)}
+                />
+                <ChartTooltip
+                  cursor={false}
+                  content={<ChartTooltipContent hideLabel indicator="line" />}
+                />
+                <Area
+                  dataKey="visitior"
+                  type="natural"
+                  strokeWidth={2}
+                  dot={false}
+                />
+                <Area
+                  dataKey="views"
+                  type="natural"
+                  strokeWidth={2}
+                  dot={false}
+                />
+              </AreaChart>
+            </ChartContainer>
+          </CardContent>
+        </CardHeader>
+      </Card>
+    </div>
+  );
 }
