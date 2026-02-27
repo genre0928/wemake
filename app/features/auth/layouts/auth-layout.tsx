edb14148757743644257d360309da3b6c2be7f98
genre0928 <genre0928@naver.com>
2026-02-27 15:34:25 +0900

    feat:auth layout Magic UI 변경

--- app/features/auth/layouts/auth-layout.tsx
+++ app/features/auth/layouts/auth-layout.tsx
@@ -1,9 +1,18 @@
 import { Outlet } from "react-router";
+import { FlickeringGrid } from "~/common/components/ui/flickering-grid";
 
 export default function AuthLayout() {
   return (
     <div className="grid grid-cols-2 h-screen gap-5">
-      <div className="bg-linear-to-br from-primary via-black to-primary/50" />
+      <div>
+        <FlickeringGrid
+          squareSize={4}
+          gridGap={5}
+          maxOpacity={0.5}
+          flickerChance={0.2}
+          color="red"
+        />
+      </div>
       <Outlet />
     </div>
   );
